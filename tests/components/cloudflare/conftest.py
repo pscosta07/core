1fb47b1df72c11f6ae9e67d95d7cbcca7b3ee32f
Chris Talkington <chris@talkingtontech.com>
2020-10-04 15:18:09 -0500

    Update conftest.py

--- tests/components/cloudflare/conftest.py
+++ tests/components/cloudflare/conftest.py
@@ -14,3 +14,16 @@ def cfupdate(hass):
         "homeassistant.components.cloudflare.CloudflareUpdater", return_value=mock_cfupdate
     ) as mock_api:
         yield mock_api
+
+
+@fixture
+def cfupdate_flow(hass):
+    """Mock the CloudflareUpdater for easier config flow testing."""
+    mock_cfupdate = _get_mock_cfupdate()
+    with patch(
+        "homeassistant.components.cloudflare.config_flow.CloudflareUpdater", return_value=mock_cfupdate
+    ) as mock_api:
+        yield mock_api
+
+
+
