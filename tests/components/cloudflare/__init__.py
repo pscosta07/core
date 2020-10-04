76216eff46cd4306ba99d279b9dd17bcad0cd026
Chris Talkington <chris@talkingtontech.com>
2020-10-04 14:30:06 -0500

    Update __init__.py

--- tests/components/cloudflare/__init__.py
+++ tests/components/cloudflare/__init__.py
@@ -1,8 +1,10 @@
 """Tests for the Cloudflare integration."""
+from pycfdns import CFRecord
+
 from homeassistant.components.cloudflare.const import CONF_RECORDS, DOMAIN
 from homeassistant.const import CONF_API_KEY, CONF_EMAIL, CONF_ZONE
 
-from tests.async_mock import patch
+from tests.async_mock import AsyncMock, patch
 from tests.common import MockConfigEntry
 
 ENTRY_CONFIG = {
@@ -26,6 +28,7 @@ YAML_CONFIG = {
     CONF_RECORDS: ["ha", "homeassistant"],
 }
 
+MOCK_ZONE = "mock.com"
 MOCK_ZONE_ID = "mock-zone-id"
 
 
@@ -43,6 +46,40 @@ async def init_integration(
     await hass.async_block_till_done()
 
     return entry
+
+
+def _get_mock_cfupdate():
+    client = AsyncMock()
+
+    zone_records = ["ha.mock.com", "homeassistant.mock.com"]
+    cf_records = [
+        CFRecord(
+            {
+                "id": "zone-record-id",
+                "type": "A",
+                "name": "ha.mock.com",
+                "proxied": True,
+                "content": "127.0.0.1",
+            }
+        ),
+        CFRecord(
+            {
+                "id": "zone-record-id-2",
+                "type": "A",
+                "name": "homeassistant.mock.com",
+                "proxied": True,
+                "content": "127.0.0.1",
+            }
+        ),
+    ]
+
+    client.get_zones = AsyncMock(return_value=[str(MOCK_ZONE)])
+    client.get_zone_records = AsyncMock(return_value=zone_records)
+    client.get_record_info = AsyncMock(return_value=cf_records)
+    client.get_zone_id = AsyncMock(return_value=str(MOCK_ZONE_ID))
+    client.update_records = AsyncMock(return_value=None)
+
+    return client
 
 
 def _patch_async_setup(return_value=True):
